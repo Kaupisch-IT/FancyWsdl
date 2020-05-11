6ae600194033bb742deb695f06b9a976c7d98091
Kaupisch-IT <github@kaupisch.it>
2020-05-11 17:29:52 +0200

    autoimplemented getters & setters

--- FancyWsdl/Program.cs
+++ FancyWsdl/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -15,19 +14,28 @@ namespace FancyWsdl
 				Encoding encoding = Encoding.UTF8;
 				string content = File.ReadAllText(path,encoding);
 
-
-				foreach (Match match in Regex.Matches(content,@"\[(?<xmlElementAttribute>System.Xml.Serialization.XmlElementAttribute\()(""(?<elementName>[^""]+)"", )?Form=System.Xml.Schema.XmlSchemaForm.Unqualified\)\]\s+public (?<propertyType>\S+)(\[\])? (?<propertyName>\S+) (?<getterSetter>\{\s+get \{\s+return [^;]+;\s+}\s+set \{\s+[^;]+;\s+}\s+})"))
+				// name in XmlElementAttribute
+				foreach (Match match in Regex.Matches(content,@"\[(?<xmlElementAttribute>System.Xml.Serialization.XmlElementAttribute\()(""(?<elementName>[^""]+)"", )?Form=System.Xml.Schema.XmlSchemaForm.Unqualified\)\]\s+public (?<propertyType>\S+) (?<propertyName>\S+)"))
 				{
 					string xmlElementAttribute = match.Groups["xmlElementAttribute"].Value;
 					string elementName = match.Groups["elementName"].Value;
 					string propertyType = match.Groups["propertyType"].Value;
 					string propertyName = match.Groups["propertyName"].Value;
-					string getterSetter = match.Groups["getterSetter"].Value;
 
 					if (String.IsNullOrEmpty(elementName))
 						content = content.Replace(match.Value,match.Value.Replace(xmlElementAttribute,xmlElementAttribute+"\""+propertyName+"\", "));
+				}
 
-					Debug.WriteLine(propertyName);
+				// autoimplemented getters & setters
+				foreach (Match match in Regex.Matches(content,@"public (?<propertyType>\S+) (?<propertyName>\S+) (?<getterSetter>\{\s+get \{\s+return [^;]+;\s+}\s+set \{\s+[^;]+;\s+}\s+})"))
+				{
+					string propertyType = match.Groups["propertyType"].Value;
+					string propertyName = match.Groups["propertyName"].Value;
+					string getterSetter = match.Groups["getterSetter"].Value;
+
+					content = content.Replace(match.Value,match.Value.Replace(getterSetter,"{ get; set; }"));
+					content = Regex.Replace(content,$"private {Regex.Escape(propertyType)} {Regex.Escape(propertyName)}Field;\\s*","");
+					// content = Regex.Replace(content,$"\\b{Regex.Escape(propertyName)}Field\\b",propertyName);
 				}
 
 				File.WriteAllText(path,content,encoding);
